f24de62ea75dd3ab80c097571b57cf2a326f8310
itbaans <kodvavi100@outlook.com>
2026-02-03 08:16:32 +0500

    fixed paths

--- scripts/push_to_hup.sh
+++ scripts/push_to_hup.sh
@@ -1,9 +1,11 @@
 OUTPUT_DIR="output/qwen2vl_test_run"
 HF_REPO="peeache/qwen2vl-gastro-vqa-test"
 MAX_STEPS=50
+CKPT_PATH=$(realpath ./output/qwen2vl_test_run/checkpoint-50)
 
 echo "Pushing to HuggingFace: ${HF_REPO}"
 swift export \
-    --adapters ./${OUTPUT_DIR}/checkpoint-${MAX_STEPS} \
+    --adapters ${CKPT_PATH} \
     --push_to_hub true \
     --hub_model_id ${HF_REPO} \
+    --use_hf true
